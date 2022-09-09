b6d187f48001296fe3398aa3aa62b85c00ab513e
Eur3ka <njuscimenphy@gmail.com>
2022-09-09 23:02:36 +0800

    v1.0.0

---
+++ main.py
@@ -0,0 +1,64 @@
+# mode 替换模式选择
+#       0 -> 不替换任何非空白字符
+#       1 -> 按替换表替换非空白字符
+def get_std_text(raw_text: str, mode: int) -> str:
+    # 去除换行符
+    std_text = raw_text.replace("\n", "")
+    if mode == 1:
+        # 替换符号
+        for (k, v) in replace_tag_map.items():
+            std_text = std_text.replace(k, v)
+    return std_text
+
+
+def get_slips(raw_text: str, length: int) -> list[list[str]]:
+    chars = [c for c in raw_text]
+    slips = [[]] * length
+    for i in range(len(chars)):
+        c = chars[i]
+        slips[i % length] = slips[i % length] + [c]
+    max_length = len(slips[0])
+    for i in range(length):
+        while len(slips[i]) < max_length:
+            slips[i] = slips[i] + [block]
+    for line in slips:
+        line.reverse()
+    return slips
+
+
+def show_slips(slips: list[list[str]]):
+    for line in slips:
+        for c in line:
+            print(c, end=" ")
+        print()
+
+
+block = "□"
+# 标点符号替换表
+replace_tag_map = {
+    ",": block,
+    ".": block,
+    "!": block,
+    "?": block,
+    "、": block,
+    "，": block,
+    "。": block,
+    "！": block,
+    "？": block
+}
+# 文本
+text = """
+元丰六年十月十二日夜，解衣欲睡，月色入户，欣然起行。
+念无与为乐者，遂至承天寺寻张怀民。
+怀民亦未寝，相与步于中庭。
+庭下如积水空明，水中藻、荇交横，盖竹柏影也。
+何夜无月？何处无竹柏？但少闲人如吾两人者耳。
+"""
+if __name__ == '__main__':
+    print("================ 赛博竹简生成器 v1.0.0 by Eur3ka ================")
+    print(text)
+    print("==>")
+    print("simple mode in 7")
+    show_slips(get_slips(get_std_text(text, 0), 7))
+    print("full mode in 7")
+    show_slips(get_slips(get_std_text(text, 1), 7))
